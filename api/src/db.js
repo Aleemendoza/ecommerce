9fc6e331cf54acb4fb95c81c64b235d94a3bd91f
Aleemendoza <Alejandromendoza.dev@gmail.com>
2020-08-04 11:06:14 -0300

    variables de ambiente y conexion a postgress

--- api/src/db.js
+++ api/src/db.js
@@ -1,10 +1,14 @@
 const Sequelize = require('sequelize');
 /* const { singularize } = require('sequelize/types/lib/utils'); */
 
+const {
+  DB_USER, DB_PASSWORD, DB_HOST,
+} = process.env;
+
 function db() {
 
-  return new Sequelize('postgres://postgres:Hitachi.11@localhost:5432/development', {
-
+  return new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecommercedevelopment`, {
+    
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   });
